d455e0cf16f6f0bedd520dd9d49e495b0a08b53c
Hamza Mahmoudi <hamzamahmoudi17@gmail.com>
2017-12-06 12:26:27 +0000

    add project to git

---
+++ ok.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	
+  </head>
+  <body>
+<div class="container">
+<div class="row">
+<div class="col-xs-12">
+<button> <span class="glyphicon glyphicon-search" ></span>search</button>
+<button class="btn btn-primary"></button>
+</div>
+</div>
+
+</div>
+<script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
